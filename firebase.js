a84fdea8bce3e2e34d26fa5b7f03a66d298d42b4
shadowwhitin-arch <shadowwhitin@gmail.com>
2026-05-12 22:46:03 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,27 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXxgO0OU9k-LvRhYY7bIuhvVpY5XgMWdk",
+  authDomain: "nuralam-90162.firebaseapp.com",
+  databaseURL: "https://nuralam-90162-default-rtdb.firebaseio.com",
+  projectId: "nuralam-90162",
+  storageBucket: "nuralam-90162.firebasestorage.app",
+  messagingSenderId: "371833786531",
+  appId: "1:371833786531:web:fc49153e527ac617612f16",
+  measurementId: "G-T16MWNKY0V"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.firebaseAuth = auth;
+window.firebaseSignIn = signInWithEmailAndPassword;
+window.firebaseOnAuthStateChanged = onAuthStateChanged;
+window.firebaseSignOut = signOut;
